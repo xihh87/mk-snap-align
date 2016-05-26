54fbadefaf4e6658006688cad4ca32adac47a5ce
Joshua Haase <hahj87@gmail.com>
2016-05-26 01:08:59 -0500

    SURPI default parameters for aligning to NT.

--- snap.mk
+++ snap.mk
@@ -1,22 +1,24 @@
-# The directory where the reference is used
-SNAP_REFERENCEDIR=''
+## The directory where the reference is used
+SNAP_REFERENCEDIR='/reference/hg19/snap_index_hg19_rRNA_mito_Hsapiens_rna/'
 
-# Here you should set aditional options for snap
-#
-# In example, SURPI defaults:
-#
-# SNAP_OPTS='-x -f -h 250 -n 25 -F u'
-SNAP_OPTS=''
+## Here you should set aditional options for snap
+##
+## In example, SURPI defaults:
+##
+SNAP_OPTS='-x -f -h 250 -n 25'
+
+## To filter unmatched regions:
+#SNAP_OPTS=$SNAP_OPTS' -F u'
 
 ## Recommended options by organism:
 ##
 ## - Human:
-#SNAP_OPTS+=' -d 12'
+SNAP_OPTS=$SNAP_OPTS' -d 12'
 
 ## Recommended cache options:
 ##
 ## - lots of memory:
-#SNAP_OPTS+=' -map'
+SNAP_OPTS=$SNAP_OPTS' -map'
 ##
 ## - less memory:
 #SNAP_OPTS+=' -pre -map'
@@ -24,4 +26,4 @@ SNAP_OPTS=''
 ## By default SNAP uses all available cores.
 ## to use less cores, uncomment the following option,
 ## where 24 is the max number of cores you want to use:
-#SNAP_OPTS+=' -t 24'
+SNAP_OPTS=$SNAP_OPTS' -t 24'
