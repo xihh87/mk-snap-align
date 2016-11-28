d3bde1d158d558b79e57d2a8df8dc9e204228d5c
hachepunto <hachepunto@gmail.com>
2016-11-28 11:01:20 -0600

    Align paired samples with reference.

--- mkfile
+++ mkfile
@@ -2,25 +2,19 @@
 # snap fails if more than one snap process is active
 NPROC=1
 
-SNAP_TARGETS=`{./snap-nt-queue}
+SNAP_TARGETS=`{./snap-targets}
 
-align-nt:V:	$SNAP_TARGETS
+align:V:	$SNAP_TARGETS
 
 # snap can filter a set of files and it's more efficient to align them all on the same run
 # rather than running surpi once for each file
 
-'results/align-nt/(.*)_(.*).sam':RD:	'data/\1'	$SNAP_REFERENCEDIR'/\2'
+'results/align/%.bam':D:	'data/%_R1.fastq.gz'	'data/%_R2.fastq.gz'
 	mkdir -p `dirname $target`
-	SNAP_FILES=`find -L \
-		data/$stem1 \
-		-name '*.fastq' \
-		-o -name '*.fastq.gz' \
-		-o -name '*.sam' \
-		-o -name '*.bam'`
 	snap-aligner \
-		single \
-		$SNAP_REFERENCEDIR/$stem2 \
-		$SNAP_FILES \
+		paired \
+		$SNAP_REFERENCE \
+		$prereq \
 		$SNAP_OPTS \
-		-o -sam - \
-		> $target
+		-o -bam $target'.build' \
+	&& mv $target'.build' $target
--- snap.mk
+++ snap.mk
@@ -1,19 +1,19 @@
 ## The directory where the reference is used
-SNAP_REFERENCEDIR='/reference/FAST_SNAP'
+SNAP_REFERENCE=$HOME'/reference/snap_index_hg19'
 
 ## Here you should set aditional options for snap
 ##
 ## In example, SURPI defaults:
 ##
-SNAP_OPTS='-x -f -h 250 -n 25'
+SNAP_OPTS='-n 25'
 
 ## Only output aligned regions:
-SNAP_OPTS=$SNAP_OPTS' -F a'
+#SNAP_OPTS=$SNAP_OPTS' -F a'
 
 ## Recommended options by organism:
 ##
 ## - Human:
-SNAP_OPTS=$SNAP_OPTS' -d 12'
+#SNAP_OPTS=$SNAP_OPTS' -d 12'
 
 ## Recommended cache options:
 ##
@@ -21,9 +21,9 @@ SNAP_OPTS=$SNAP_OPTS' -d 12'
 #SNAP_OPTS=$SNAP_OPTS' -map'
 ##
 ## - reference not in cache:
-SNAP_OPTS+=' -pre -map'
+SNAP_OPTS=$SNAP_OPTS' -pre -map'
 
 ## By default SNAP uses all available cores.
 ## to use less cores, uncomment the following option,
 ## where 24 is the max number of cores you want to use:
-SNAP_OPTS=$SNAP_OPTS' -t 24'
+SNAP_OPTS=$SNAP_OPTS' -t 20'
